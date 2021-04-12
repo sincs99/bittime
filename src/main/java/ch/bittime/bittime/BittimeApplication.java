4a677ad1aa6ff91b9e9f6c1a205e536d9948bcb0
pascal <pascal.kone@bluewin.ch>
2021-04-12 21:36:39 +0200

    Initial Commit

---
+++ src/main/java/ch/bittime/bittime/BittimeApplication.java
@@ -0,0 +1,15 @@
+package ch.bittime.bittime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BittimeApplication {
+
+	public String test;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BittimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bittime/bittime/BittimeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bittime.bittime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BittimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
