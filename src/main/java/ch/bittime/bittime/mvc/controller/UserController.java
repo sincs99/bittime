e27a82d36d4a3b28eff518f7bcaef8fc5dc9c2f0
André <andre.kaufmann@students.fhnw.ch>
2021-04-26 15:06:47 +0200

    added UserController todo's

--- src/main/java/ch/bittime/bittime/mvc/controller/UserController.java
+++ src/main/java/ch/bittime/bittime/mvc/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 
 public class UserController {
+
         @Autowired
         private UserService userService;
         @Autowired
