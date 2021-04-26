99492e1c103986d3104ab84f2917e6c96a199f8b
André <andre.kaufmann@students.fhnw.ch>
2021-04-26 16:37:00 +0200

    created user views + controller

--- src/main/java/ch/bittime/bittime/mvc/controller/UserController.java
+++ src/main/java/ch/bittime/bittime/mvc/controller/UserController.java
@@ -1,58 +1,32 @@
 package ch.bittime.bittime.mvc.controller;
 
-import ch.bittime.bittime.login.UserService;
-import ch.bittime.bittime.login.repository.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author Andre
  */
 
+@Controller
 public class UserController {
 
-        @Autowired
-        private UserService userService;
-        @Autowired
-        private UserRepo userRepo;
+        @GetMapping("/user/timeRecording")
+        public String timeRecording(){
+            return "/user/timeRecording";
+        }
 
-    ////    @RequestMapping(value = "admin/deleteUser", method = RequestMethod.DELETE)
-    ////    public String deleteUser(Model model){
-    ////        List<User> listUser =userRepo.finb
-    ////    }
-    //
-    //
-    //    @GetMapping("/admin/userPanel")
-    //    public String listUser(Model model){
-    //        List<User> listUser = userRepo.findAll();
-    //        model.addAttribute("listUser", listUser);
-    //        System.out.println(listUser);
-    //
-    //        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    //        User user = userService.findUserByUserName(auth.getName());
-    //        model.addAttribute("userName", "Welcome " + user.getUserName() + "/" + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-    //
-    //        return "/admin/userPanel";
-    //
-    //    }
-    //
-    //    @GetMapping("/admin/timeRecording")
-    //    public String timeRecording(){
-    //        return "/admin/timeRecording";
-    //    }
-    //
-    //    @GetMapping("/admin/reportingView")
-    //    public String reportingView(){
-    //        return "/admin/reportingView";
-    //    }
-    //
-    //    @GetMapping("/admin/vacationManagement")
-    //    public String vacationManagement(){
-    //        return "/admin/vacationManagement";
-    //    }
-    //
-    //    @GetMapping("/admin/profileView")
-    //    public String profileView(){
-    //        return "/admin/profileView";
-    //    }
+        @GetMapping("/user/vacationView")
+        public String vacationView(){ return "/user/vacationView";
+    }
+
+        @GetMapping("/user/reportingView")
+        public String reportingView(){
+            return "/user/reportingView";
+        }
+
+        @GetMapping("/user/profileView")
+        public String profileView(){
+            return "/user/profileView";
+        }
 
 }
