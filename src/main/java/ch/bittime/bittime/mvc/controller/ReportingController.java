556b5e2baec5f8cf7942fff3eb73b65bc3ad2b1d
André <andre.kaufmann@students.fhnw.ch>
2021-05-03 16:29:28 +0200

    Next try

---
+++ src/main/java/ch/bittime/bittime/mvc/controller/ReportingController.java
@@ -0,0 +1,58 @@
+package ch.bittime.bittime.mvc.controller;
+
+import ch.bittime.bittime.login.User;
+import ch.bittime.bittime.login.UserService;
+import ch.bittime.bittime.login.repository.UserRepo;
+import ch.bittime.bittime.reporting.ReportingService;
+import com.lowagie.text.DocumentException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.*;
+import java.util.List;
+import java.util.Optional;
+
+
+/**
+ * @author André
+ */
+
+//thanks to:
+//https://artificialintelligence.oodles.io/dev-blogs/Tutorial-for-Generating-Dynamic-PDFs-Using-Thymeleaf
+//https://tuhrig.de/generating-pdfs-with-java-flying-saucer-and-thymeleaf/
+//https://github.com/tuhrig/Flying_Saucer_PDF_Generation/blob/master/src/test/java/FlyingSaucerTest.java
+
+@Controller
+public class ReportingController {
+
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private ReportingService reportingService;
+
+    @RequestMapping("/generate")
+    public String generatePdf(Model model) throws IOException, DocumentException {
+
+        ReportingService thymeleaf2Pdf = new ReportingService();
+        String html = thymeleaf2Pdf.parseThymeleafTemplate();
+        thymeleaf2Pdf.generatePdfFromHtml(html);
+        System.out.println("here's the PDF");
+
+        return "/admin/reportingView";
+    }
+
+}
---
+++ src/main/java/ch/bittime/bittime/reporting/ReportingService.java
@@ -0,0 +1,50 @@
+package ch.bittime.bittime.reporting;
+
+import com.lowagie.text.DocumentException;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import javax.transaction.Transactional;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author André
+ */
+
+@Service
+@Transactional
+public class ReportingService {
+
+    public String parseThymeleafTemplate() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("data", "data");
+
+        return templateEngine.process("thymeleaf_template", context);
+    }
+
+    public void generatePdfFromHtml(String html) throws IOException, DocumentException {
+        String outputFolder = System.getProperty("user.home") + File.separator + "thymeleaf.pdf";
+
+        OutputStream outputStream = new FileOutputStream(outputFolder);
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(html);
+        renderer.layout();
+        renderer.createPDF(outputStream);
+
+        outputStream.close();
+    }
+}
